d348e61f6835fd3487d9b70c4b67c47cc8528d91
laizuqi <laizhuqi1111@163.com>
2018-11-20 16:59:40 +0800

    使用远程zookeeper
    使用远程zookeeper

--- dubbo-springboot-demo/demo-dubbo-consumer/src/main/java/com/dubbo/client/controller/UserController.java
+++ dubbo-springboot-demo/demo-dubbo-consumer/src/main/java/com/dubbo/client/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.dubbo.client.controller;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +21,8 @@ public class UserController {
 	@Reference
 	UserService userService;
 
+	/*@GetMapping是一个组合注解，是@RequestMapping(method = RequestMethod.GET)的缩写。
+	 * @PostMapping是一个组合注解，是@RequestMapping(method = RequestMethod.POST)的缩写。*/
 	@GetMapping("/findById/{id}")
 	public UserDO findById(@PathVariable Long id) {
 		return userService.findById(id);
@@ -25,5 +31,22 @@ public class UserController {
 	@GetMapping("/listUser")
 	public List<UserDO> listUser() {
 		return userService.listUser();
+	}
+	
+	@GetMapping("/login")
+	public String UserLogin(HttpServletRequest req,HttpServletResponse res) throws IOException {
+		String userName=req.getParameter("name");
+		String userPasswd=req.getParameter("passwd");
+		if(userName.equals("lzq")&&userPasswd.equals("lzq")){
+			res.sendRedirect("http://47.107.145.108/loginsucc.html");
+			System.out.println("Hi,"+userName+",you are login succ!");
+			return "login_succ";
+		}
+		else{
+			res.sendRedirect("http://47.107.145.108/loginfail.html");
+			System.out.println("Hei,"+userName+",you are login fail!");
+			return "login_fail";
+		}
+		
 	}
 }
